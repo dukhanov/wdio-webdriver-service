714f2e852aa4d8382ae4e542643baa7896a4bd61
Maxim Dukhanov <dukhanov@skycom.com>
2019-06-12 10:26:50 +0300

    Updated package JSON

--- test/integration/main.spec.js
+++ test/integration/main.spec.js
@@ -5,10 +5,10 @@ var path = require('path')
 describe('webdriverio', function () {
     it('should run a test', function () {
         browser.url('/')
-        assert.equal(browser.getTitle(), 'WebdriverIO - WebDriver bindings for Node.js')
+        assert.equal(browser.getTitle(), 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
 
         browser.click('a=API')
-        assert.equal(browser.getTitle(), 'WebdriverIO - API Docs')
+        assert.equal(browser.getTitle(), 'API Docs · WebdriverIO')
     })
 
     it('should have created a log file', function () {
